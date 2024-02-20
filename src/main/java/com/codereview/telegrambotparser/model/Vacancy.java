19b185b7868e549e3463462acefeefd474a1db03
IvanSalovProg <ivansalov94@gmail.com>
2024-02-20 15:44:50 +0500

    1
    edit db PostgreSql

--- src/main/java/com/codereview/telegrambotparser/model/Vacancy.java
+++ src/main/java/com/codereview/telegrambotparser/model/Vacancy.java
@@ -1,9 +1,25 @@
 package com.codereview.telegrambotparser.model;
 
+//import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
+@Getter
+@Setter
+@Entity(name ="Vacancy")
 public class Vacancy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
     private int position;
     private String name;
     private String company;
